2315a3a312599af0e8d4e0716894920f2d499eff
Samm <44311301+KrAsH-CoD3@users.noreply.github.com>
2025-08-25 02:52:37 +0100

    Updated: Test file(removed issues permission, added pr permission)

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 // This is a test file for PR workflow trigger
-// Testing to see if workflow files works without pull-request permission
+// Testing to see if workflow files works with pull-request permission
+// And also after removing issues write permission
